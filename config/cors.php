3073a857f81824e47ce9304954e1d17696fd943f
Melindes prime <wilfriedmelindes13@gmail.com>
2026-06-06 11:20:34 +0000

    fix: configuration CORS production

---
+++ config/cors.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'paths'                    => ['api/*'],
+    'allowed_methods'          => ['*'],
+    'allowed_origins'          => [
+        'http://localhost:5173',
+        'https://todo-app-frontend-red-alpha.vercel.app',
+    ],
+    'allowed_origins_patterns' => [],
+    'allowed_headers'          => ['*'],
+    'exposed_headers'          => [],
+    'max_age'                  => 0,
+    'supports_credentials'     => false,
+];
